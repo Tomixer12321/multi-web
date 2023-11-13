707ebe4aee8db288c27e41acf71fcd1aada10bce
Tomixer <tomix.klokar@gmail.com>
2023-11-13 17:31:07 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return<BrowserRouter>
+   <Routes>
+    <Route path="/" element={<h1>home</h1>}/>
+    <Route path="/movies" element={<h1>filmy</h1>}/>
+    <Route path="/serials" element={<h1>serialy</h1>}/>
+    <Route path="new" element={<h1>nove</h1>}/>
+   </Routes>
+   </BrowserRouter>
 }
 
 export default App
