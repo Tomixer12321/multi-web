2dfb65f3cf119ed6be49d963eeee5146f20046f2
Tomixer <tomix.klokar@gmail.com>
2023-11-17 20:18:59 +0100

    useParams

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -1,6 +1,13 @@
+import { useParams } from "react-router-dom"
+
 const OneMovie = () => {
+
+    console.log(useParams())
+
+    const {movieID}=useParams()
+
   return <section>
-    <h1>jeden film</h1>
+    <h1>{movieID}</h1>
   </section>
 }
 
