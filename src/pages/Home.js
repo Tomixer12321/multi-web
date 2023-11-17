00db260ed0bdb5aa428479e4fcc83df055d9d380
Tomixer <tomix.klokar@gmail.com>
2023-11-17 14:54:43 +0100

    male upravy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,3 @@
-import { Link, Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
 const Home = () => {
   return (
     <section>
