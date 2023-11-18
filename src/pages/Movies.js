559c0afd57677e421cecbab28735e18058b5636d
Tomixer <tomix.klokar@gmail.com>
2023-11-18 10:53:40 +0100

    vypisanie filmov do stranky

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import movies from "../data";
+
 const Movies = () => {
   return (
     <section>
       <h1>Filmy</h1>
+      <div>
+        {movies.map((oneMovie) => {
+          return (
+            <article key={oneMovie.id}>
+              <h2>{oneMovie.title}</h2>
+              <img src={oneMovie.image} alt="" />
+              <br />
+              <Link to={`/all-movies/${oneMovie.id}`}>vyce informaci</Link>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
